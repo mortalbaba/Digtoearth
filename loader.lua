89e3b4149bf811ffe14b9c215f2a87c1a4e882d9
mortalbaba <alicankalkan963@gmail.com>
2025-07-03 21:06:58 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,68 +1,61 @@
--- Gui Script by MortalHere
-local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local ToggleButton = Instance.new("TextButton")
-local SpinButton = Instance.new("TextButton")
-local CoinButton = Instance.new("TextButton")
-local PetButton = Instance.new("TextButton")
-local dragging, dragInput, dragStart, startPos
+local a=Instance.new("\83\99\114\101\101\110\71\117\105")
+local b=Instance.new("\70\114\97\109\101")
+local c=Instance.new("\84\101\120\116\66\117\116\116\111\110")
+local d=Instance.new("\84\101\120\116\66\117\116\116\111\110")
+local e=Instance.new("\84\101\120\116\66\117\116\116\111\110")
+local f=Instance.new("\84\101\120\116\66\117\116\116\111\110")
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-ScreenGui.Name = "CustomGui"
+a.Parent=game.Players.LocalPlayer:WaitForChild("\80\108\97\121\101\114\71\117\105")
+a.Name="\67\117\115\116\111\109\71\117\105"
 
--- Frame (Ana Panel)
-Frame.Name = "Main"
-Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-Frame.Position = UDim2.new(0.3, 0, 0.3, 0)
-Frame.Size = UDim2.new(0, 250, 0, 200)
-Frame.Active = true
-Frame.Draggable = true
+b.Name="\77\97\105\110"
+b.Parent=a
+b.BackgroundColor3=Color3.fromRGB(25,25,25)
+b.Position=UDim2.new(0.3,0,0.3,0)
+b.Size=UDim2.new(0,250,0,200)
+b.Active=true
+b.Draggable=true
 
--- Toggle Button
-ToggleButton.Name = "Toggle"
-ToggleButton.Parent = ScreenGui
-ToggleButton.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-ToggleButton.Position = UDim2.new(0, 10, 0, 10)
-ToggleButton.Size = UDim2.new(0, 100, 0, 40)
-ToggleButton.Text = "Toggle Menu"
-ToggleButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-ToggleButton.MouseButton1Click:Connect(function()
-	Frame.Visible = not Frame.Visible
+c.Name="\84\111\103\103\108\101"
+c.Parent=a
+c.BackgroundColor3=Color3.fromRGB(50,50,50)
+c.Position=UDim2.new(0,10,0,10)
+c.Size=UDim2.new(0,100,0,40)
+c.Text="\84\111\103\103\108\101\32\77\101\110\117"
+c.TextColor3=Color3.fromRGB(255,255,255)
+c.MouseButton1Click:Connect(function()
+	b.Visible=not b.Visible
 end)
 
--- Infinite Spin Button
-SpinButton.Name = "Spin"
-SpinButton.Parent = Frame
-SpinButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
-SpinButton.Position = UDim2.new(0.1, 0, 0.1, 0)
-SpinButton.Size = UDim2.new(0, 200, 0, 40)
-SpinButton.Text = "Infinite Spin"
-SpinButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-SpinButton.MouseButton1Click:Connect(function()
-	game:GetService("ReplicatedStorage").Remotes.SpinPrizeEvent:FireServer(10)
+d.Name="\83\112\105\110"
+d.Parent=b
+d.BackgroundColor3=Color3.fromRGB(100,100,100)
+d.Position=UDim2.new(0.1,0,0.1,0)
+d.Size=UDim2.new(0,200,0,40)
+d.Text="\73\110\102\105\110\105\116\101\32\83\112\105\110"
+d.TextColor3=Color3.fromRGB(255,255,255)
+d.MouseButton1Click:Connect(function()
+	game:GetService("\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101").Remotes.SpinPrizeEvent:FireServer(10)
 end)
 
--- x10 Coin Button
-CoinButton.Name = "x10Coin"
-CoinButton.Parent = Frame
-CoinButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
-CoinButton.Position = UDim2.new(0.1, 0, 0.4, 0)
-CoinButton.Size = UDim2.new(0, 200, 0, 40)
-CoinButton.Text = "Add x10 Coin"
-CoinButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-CoinButton.MouseButton1Click:Connect(function()
-	game:GetService("ReplicatedStorage").Remotes.SpinPrizeEvent:FireServer(8)
+e.Name="\120\49\48\67\111\105\110"
+e.Parent=b
+e.BackgroundColor3=Color3.fromRGB(100,100,100)
+e.Position=UDim2.new(0.1,0,0.4,0)
+e.Size=UDim2.new(0,200,0,40)
+e.Text="\65\100\100\32\120\49\48\32\67\111\105\110"
+e.TextColor3=Color3.fromRGB(255,255,255)
+e.MouseButton1Click:Connect(function()
+	game:GetService("\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101").Remotes.SpinPrizeEvent:FireServer(8)
 end)
 
--- Give Op Pet Button
-PetButton.Name = "OpPet"
-PetButton.Parent = Frame
-PetButton.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
-PetButton.Position = UDim2.new(0.1, 0, 0.7, 0)
-PetButton.Size = UDim2.new(0, 200, 0, 40)
-PetButton.Text = "Give Op Pet"
-PetButton.TextColor3 = Color3.fromRGB(255, 255, 255)
-PetButton.MouseButton1Click:Connect(function()
-	game:GetService("ReplicatedStorage").Remotes.SpinPrizeEvent:FireServer(4)
+f.Name="\79\112\80\101\116"
+f.Parent=b
+f.BackgroundColor3=Color3.fromRGB(100,100,100)
+f.Position=UDim2.new(0.1,0,0.7,0)
+f.Size=UDim2.new(0,200,0,40)
+f.Text="\71\105\118\101\32\79\112\32\80\101\116"
+f.TextColor3=Color3.fromRGB(255,255,255)
+f.MouseButton1Click:Connect(function()
+	game:GetService("\82\101\112\108\105\99\97\116\101\100\83\116\111\114\97\103\101").Remotes.SpinPrizeEvent:FireServer(4)
 end)
